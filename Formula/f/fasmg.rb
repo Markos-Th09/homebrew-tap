481d431aab31af5382241af46bb660d597a2c381
MarkosTh09 <markos.theocharis2009@gmail.com>
2025-05-30 18:23:11 +0400

    fasmg: g.ktge

--- Formula/f/fasmg.rb
+++ Formula/f/fasmg.rb
@@ -1,9 +1,9 @@
 class Fasmg < Formula
   desc "New assembly engine designed as a successor of the one used by flat assembler 1"
   homepage "https://flatassembler.net"
-  url "https://flatassembler.net/fasmg.kp60.zip"
-  version "kp60"
-  sha256 "cd18f546c04007226fe0ce5bea874d14382911ed4f5a1995f6bde913e7e4e751"
+  url "https://flatassembler.net/fasmg.ktge.zip"
+  version "g.ktge"
+  sha256 "09af376d49c786d11788584687d479652584e8d48a26e04cadb6ec0ad3fe1deb"
   license "BSD-3-Clause"
   head "https://github.com/tgrysztar/fasmg.git", branch: "master"
 
@@ -21,11 +21,16 @@ class Fasmg < Formula
     elsif OS.mac?
       os = "macos"
       fasmg = "./source/#{os}/x64/fasmg"
+    else
+      odie "Unsupported operating system"
     end
 
+    cd "core" if build.head?
     chmod "+x", fasmg
+
     system fasmg, "./source/#{os}/x64/fasmg.asm", "./fasmg"
     chmod "+x", "fasmg"
+
     bin.install "fasmg"
     doc.install Dir["docs/*"]
     (pkgshare/"examples").install Dir["examples/*"]
@@ -33,60 +38,46 @@ class Fasmg < Formula
 
   test do
     ENV["INCLUDE"] = pkgshare/"examples/x86/include"
+    format = OS.mac? ? "MachO64" : "ELF64 executable"
+    entry = OS.mac? ? "_main" : "_start"
+    modifier = OS.mac? ? "public" : "entry"
+    strings_section = OS.mac? ? "section '__cstring' align 1" : "segment readable"
+    text_section = OS.mac? ? "section '__text' align 16" : "segment readable executable"
+
+    (testpath/"hello.asm").write <<~EOS
+      include 'format/format.inc'
+      format #{format}
+      #{modifier} #{entry}
+
+      SYS_write = #{OS.mac? ? "0x2000004" : "1"}
+      SYS_exit = #{OS.mac? ? "0x2000001" : "60"}
+
+      #{OS.mac? ? "segment '__TEXT' readable executable" : ""}
+      #{strings_section}
+      message db 'Hello, world!', 0x0A
+      message_len = $ - message
+
+      #{text_section}
+      #{entry}:
+        mov rax, SYS_write
+        mov rdi, 1
+        lea rsi, [message]
+        mov rdx, message_len
+        syscall
+        mov rax, SYS_exit
+        xor rdi, rdi
+        syscall
+        ret
+    EOS
 
     if OS.mac?
-      (testpath/"hello.asm").write <<~EOS
-        include 'format/format.inc'
-        format MachO64
-        public _main
-
-        segment '__TEXT' readable executable
-
-        section '__cstring' align 1
-        message db 'Hello, world!', 0x0A
-        message_len = $ - message
-
-        section '__text' align 16
-        _main:
-          mov rax, 0x2000004
-          mov rdi, 1
-          lea rsi, [message]
-          mov rdx, message_len
-          syscall
-          mov rax, 0x2000001
-          xor rdi, rdi
-          syscall
-          ret
-      EOS
       system bin/"fasmg", "hello.asm", "hello.o"
-      assert_path_exists testpath/"hello.o"
       system ENV.cc, "hello.o", "-o", "hello"
     elsif OS.linux?
-      (testpath/"hello.asm").write <<~EOS
-        include 'format/format.inc'
-        format ELF64 executable 3
-        entry _start
-
-        segment readable executable
-        _start:
-          mov rax, 1
-          mov rdi, 1
-          mov rsi, message
-          mov rdx, message_len
-          syscall
-          mov rax, 60
-          xor rdi, rdi
-          syscall
-
-        segment readable
-          message db 'Hello, world!', 0x0A
-          message_len = $ - message
-      EOS
       system bin/"fasmg", "hello.asm", "hello"
     end
 
     chmod "+x", "hello"
-    assert_path_exists testpath/"hello"
     assert_equal "Hello, world!\n", shell_output("./hello")
   end
 end
